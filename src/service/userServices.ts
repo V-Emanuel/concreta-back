3e5c402e5f58c17cba90c146adcdaeb693b752e7
vitaum <gvictor567@gmail.com>
2024-04-13 21:58:39 -0300

    feat: models

--- src/service/userServices.ts
+++ src/service/userServices.ts
@@ -5,8 +5,9 @@ import bcrypt from "bcrypt";
 async function create(body: UserCreate): Promise<void> {
     // const { rowCount:  } = await userRepositories.findByEmail(body.email);
     // if (rowCount) throw errors.duplicatedEmailError(body.email);
-    const hashPassword = await bcrypt.hash(body.password, 10);
-    await userRepositories.create(body);
+    const hashPassword: string = await bcrypt.hash(body.password, 10);
+
+    await userRepositories.create({ name: body.name, email: body.email, password: hashPassword, user_status: body.user_status });
 }
 
 export default {
